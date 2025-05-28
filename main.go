ed2b3929927c101c39b553b5059dbd6708043532
estreg <gerstmann.p@googlemail.com>
2025-05-28 11:17:21 +0200

    added firmlogo and a healthz check for ext. sys

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness) // register the Readiness func/handler; a tool so an ext. sys can check if our serv is running
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -20,4 +21,10 @@ func main() {
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK))) // If [ResponseWriter.WriteHeader] has not yet been called, Write calls WriteHeader(http.StatusOK) before writing the data.
 }
